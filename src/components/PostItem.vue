c08ad906c6eb06562b1a1acf097fd8320a9170c3
Egor Dragel <metamorgasd@gmail.com>
2022-11-06 22:00:00 +0300

    init: deploy project

---
+++ src/components/PostItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="post">
+        <div>
+            <div>{{post.id}} </div>
+            <div><strong>Название:</strong> {{post.title}} </div>
+            <div><strong>Описание:</strong> {{post.body}} </div>
+        </div>
+        <div class="post__btns">
+            <my-button
+            @click="$router.push(`/posts/${post.id}`)"
+            >
+                Открыть
+            </my-button>
+
+            <my-button
+            @click="$emit('remove', post)"
+            >
+                Удалить
+            </my-button>
+        </div>
+    </div>
+</template>
+<script>
+import router from '@/router/router';
+
+export default {
+    props:{
+        post:{
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+<style scoped>
+    .post{
+		margin-top: 10px;
+		padding: 15px;
+		border: 2px solid teal;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+	}
+    .post__btns{
+        display: flex;
+    }
+</style>
---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+<script>
+export default {
+    name: 'my-button'
+}
+</script>
+<style scoped>
+.btn{
+	border: 2px solid teal;
+	padding: 10px 15px;
+	background: none;
+	color: teal;	
+	}
+</style>
